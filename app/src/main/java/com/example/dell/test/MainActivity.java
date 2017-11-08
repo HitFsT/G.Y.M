b2d492f1e196e33086e96ce18b623c2634194661
HitFsT <396433458@qq.com>
2017-11-08 22:08:12 +0800

    学生登录界面基本完成

--- app/src/main/java/com/example/dell/test/MainActivity.java
+++ app/src/main/java/com/example/dell/test/MainActivity.java
@@ -15,12 +15,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+//    /** Called when the user taps the Send button */
+//    public void sendMessage(View view) {
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//    }
+
+    public void StudentLogin(View view) {
+        Intent intent = new Intent(this, StudentLoginActivity.class);
         startActivity(intent);
     }
 
---
+++ app/src/main/java/com/example/dell/test/StudentLoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.dell.test;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class StudentLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_login);
+    }
+
+    public void StudentLogin(View view) {
+        Intent intent = new Intent(this, StudentActivity.class);
+        startActivity(intent);
+    }
+}
