9796fe6a5cbf25f9ef5eeac21855ca0d44f0abd7
HitFsT <396433458@qq.com>
2017-11-15 11:46:10 +0800

    界面全部完成

---
+++ app/src/main/java/com/example/dell/test/AddGameActivity.java
@@ -0,0 +1,56 @@
+package com.example.dell.test;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+
+public class AddGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_game);
+    }
+
+    public void AddGame(View view) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(AddGameActivity.this);
+        dialog.setTitle("添加信息");
+        dialog.setMessage("添加成功");
+        dialog.setCancelable(false);
+        dialog.setPositiveButton("OK", new DialogInterface.
+                OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(AddGameActivity.this, StaffGameActivity.class);
+                startActivity(intent);
+            }
+        });
+        dialog.show();
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {//当返回按键被按下
+            AlertDialog.Builder dialog = new AlertDialog.Builder(AddGameActivity.this);
+            dialog.setTitle("退出添加");
+            dialog.setMessage("确定取消添加吗?");
+            dialog.setCancelable(false);
+            dialog.setPositiveButton("OK", new DialogInterface.
+                    OnClickListener() {
+                public void onClick(DialogInterface dialog, int which)  {
+                    Intent intent = new Intent(AddGameActivity.this, GymStaffActivity.class);
+                    startActivity(intent);
+                }
+            });
+            dialog.setNegativeButton("Cancel", new DialogInterface.
+                    OnClickListener() {
+                public void onClick(DialogInterface dialog, int which)  {
+                }
+            });
+            dialog.show();
+        }
+        return false;
+    }
+}
